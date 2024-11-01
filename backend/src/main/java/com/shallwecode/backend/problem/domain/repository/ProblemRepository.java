bb3a8814ef0e32f1be3c4b294ff06fbc184a66ee
aosskfdlrla <gmltns885@gmail.com>
2024-11-01 14:53:36 +0900

    #9 feat JPARespository 추가 - Problem - Testcase

---
+++ backend/src/main/java/com/shallwecode/backend/problem/domain/repository/ProblemRepository.java
@@ -0,0 +1,8 @@
+package com.shallwecode.backend.problem.domain.repository;
+
+import com.shallwecode.backend.problem.domain.aggregate.Problem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProblemRepository extends JpaRepository<Problem, Integer> {
+
+}
---
+++ backend/src/main/java/com/shallwecode/backend/problem/domain/repository/TestcaseRepository.java
@@ -0,0 +1,8 @@
+package com.shallwecode.backend.problem.domain.repository;
+
+import com.shallwecode.backend.problem.domain.aggregate.Testcase;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TestcaseRepository extends JpaRepository<Testcase, Integer> {
+
+}
