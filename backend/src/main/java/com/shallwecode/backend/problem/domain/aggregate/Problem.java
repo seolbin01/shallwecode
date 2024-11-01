90f3ab95f8e870d1132e23926df5e2b4b359b06b
신민철 <dbeoalscjf98@naver.com>
2024-11-01 17:07:46 +0900

    feat: Problem, Testcase 엔티티 기본키 데이터 타입 Long으로 수정 및 setter 메소드 추가 #6

--- backend/src/main/java/com/shallwecode/backend/problem/domain/aggregate/Problem.java
+++ backend/src/main/java/com/shallwecode/backend/problem/domain/aggregate/Problem.java
@@ -1,14 +1,31 @@
 package com.shallwecode.backend.problem.domain.aggregate;
 
 import jakarta.persistence.*;
+import lombok.Getter;
 
 @Entity
 @Table(name = "problem")
+@Getter
 public class Problem {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int problemId;
+    private Long problemId;
     private String title;
     private String content;
-    private int problemLevel;
+    private Integer problemLevel;
+
+    public void updateProblemTitle(String title) {
+        this.title = title;
+    }
+
+    public void updateProblemContent(String content) {
+        this.content = content;
+    }
+
+    public void updateProblemProblemLevel(int problemLevel) {
+        this.problemLevel = problemLevel;
+    }
+
+
+
 }
