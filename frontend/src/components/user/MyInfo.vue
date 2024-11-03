327f39cfeffd6aca6615d013fadc4f3a236a4490
seolbin01 <se01bin@suwon.ac.kr>
2024-11-03 22:40:18 +0900

    feat: 마이페이지 사이드바 추가

---
+++ frontend/src/components/user/MyInfo.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+  <h1>회원 정보 컴포넌트입니다.</h1>
+</template>
+
+<style scoped>
+
+</style>
--- frontend/src/views/user/MyPageTap.vue
+++ frontend/src/views/user/MyPageTap.vue
@@ -1,11 +1,81 @@
 <script setup>
+import {ref} from "vue";
+import MyInfo from "@/components/user/MyInfo.vue";
 
+const selectedMenu = ref('friends');
+
+const selectMenu = (menu) => {
+  selectedMenu.value = menu;
+};
 </script>
 
 <template>
-  <h1>유저탭</h1>
+  <div class="padding">
+    <div class="menu-container">
+      <div class="menu-button" :class="{ 'active': selectedMenu === 'myinfo' }" @click="selectMenu('myinfo')">
+        <a>회원 정보</a>
+      </div>
+      <div class="menu-button" :class="{ 'active': selectedMenu === 'friend' }" @click="selectMenu('friend')">
+        <a>친구 관리</a>
+      </div>
+      <div class="menu-button" :class="{ 'active': selectedMenu === 'noti' }" @click="selectMenu('noti')">
+        <a>알림 목록</a>
+      </div>
+      <div class="menu-button" :class="{ 'active': selectedMenu === 'problem' }" @click="selectMenu('problem')">
+        <a>풀이 문제 목록</a>
+      </div>
+      <div class="menu-button" :class="{ 'active': selectedMenu === 'codingRoom' }" @click="selectMenu('codingRoom')">
+        <a>참여중인 코딩방 목록</a>
+      </div>
+    </div>
+
+    <div class="menu-content">
+      <MyInfo v-if="selectedMenu === 'myinfo'" />
+      <MyFriendManagement v-else-if="selectedMenu === 'friend'" />
+      <MyNotiList v-else-if="selectedMenu === 'noti'" />
+      <MyProblemList v-else-if="selectedMenu === 'problem'" />
+      <MyCodingRoomList v-else-if="selectedMenu === 'codingRoom'" />
+    </div>
+  </div>
 </template>
 
 <style scoped>
+.padding {
+  display: flex;
+}
 
+.menu-container {
+  width: 220px;
+  background: #ffffff;
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
+  padding: 8px;
+  margin: 30px;
+  border: 1px solid var(--gray-line);
+}
+
+.menu-button {
+  font-size: 16px;
+  font-weight: 600;
+  color: var(--black);
+  cursor: pointer;
+  padding: 12px 16px;
+}
+
+.menu-content {
+  width: 1035px;
+  background-color: var(--background-color);
+}
+
+.menu-button:hover {
+  background-color: var(--hover);
+}
+
+.menu-button:active {
+  background-color: var(--active);
+}
+
+.menu-button:not(:last-child) {
+  border-bottom: 1px solid var(--gray-line);
+}
 </style>
