e4c2f3974f83c58b2fe3a5e13ffbc0de6c5ca8b5
leebackcoding <lcy2825@gmail.com>
2024-11-04 17:12:32 +0900

    feat: 친구 신청 수락, 거절에 대한 controller 구현

--- backend/src/main/java/com/shallwecode/backend/user/application/controller/FriendController.java
+++ backend/src/main/java/com/shallwecode/backend/user/application/controller/FriendController.java
@@ -1,11 +1,11 @@
 package com.shallwecode.backend.user.application.controller;
 
-import com.shallwecode.backend.common.exception.CustomException;
-import com.shallwecode.backend.common.exception.ErrorCode;
 import com.shallwecode.backend.user.application.dto.SaveFriendReqDTO;
+import com.shallwecode.backend.user.application.dto.UpdateFriendReqDTO;
 import com.shallwecode.backend.user.application.service.FriendService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,8 +23,22 @@ public class FriendController {
 
     @Operation(summary = "친구 신청", description = "친구를 신청한다")
     @PostMapping("/request")
-    public ResponseEntity<Void> saveFriend(@RequestBody SaveFriendReqDTO saveFriendReqDTO) {
+    public ResponseEntity<Void> saveFriend(@Valid @RequestBody SaveFriendReqDTO saveFriendReqDTO) {
         friendService.saveFriend(saveFriendReqDTO);
+        return ResponseEntity.ok().build();
+    }
+
+    @Operation(summary = "친구 신청 수락", description = "친구 신청을 수락한다.")
+    @PostMapping("/request/accept")
+    public ResponseEntity<Void> acceptFriend(@Valid @RequestBody UpdateFriendReqDTO updateFriendReqDTO) {
+        friendService.updateFriend(updateFriendReqDTO, true);
+        return ResponseEntity.ok().build();
+    }
+
+    @Operation(summary = "친구 신청 거절", description = "친구 신청을 거절한다.")
+    @PostMapping("/request/reject")
+    public ResponseEntity<Void> rejectFriend(@Valid @RequestBody UpdateFriendReqDTO updateFriendReqDTO) {
+        friendService.updateFriend(updateFriendReqDTO, false);
         return ResponseEntity.ok().build();
     }
 }
