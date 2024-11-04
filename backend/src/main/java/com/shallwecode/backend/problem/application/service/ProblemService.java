c90701e0a955dee1a1b75cdb2fde9da398f5418b
aosskfdlrla <gmltns885@gmail.com>
2024-11-04 15:06:49 +0900

    feat: ProblemService 메소드 추가 #33

--- backend/src/main/java/com/shallwecode/backend/problem/application/service/ProblemService.java
+++ backend/src/main/java/com/shallwecode/backend/problem/application/service/ProblemService.java
@@ -1,10 +1,32 @@
 package com.shallwecode.backend.problem.application.service;
 
+import com.shallwecode.backend.problem.application.dto.ProblemReqDTO;
+import com.shallwecode.backend.problem.application.dto.ProblemResDTO;
+import com.shallwecode.backend.problem.domain.service.ProblemDomainService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ProblemService {
 
+    private final ProblemDomainService problemDomainService;
+
+    public List<ProblemResDTO> selectOneProblem(Long problemId) {
+        return problemDomainService.selectOneProblem(problemId);
+    }
+
+    public void deleteProblem(Long problemId) {
+        problemDomainService.deleteProblem(problemId);
+    }
+
+    public void updateProblem(Long problemId, ProblemReqDTO newProblemInfo) {
+        problemDomainService.updateProblem(problemId, newProblemInfo);
+    }
+
+    public void saveProblem(ProblemReqDTO newProblemInfo) {
+        problemDomainService.saveProblem(newProblemInfo);
+    }
 }
