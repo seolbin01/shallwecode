fc995186d3db401a1d013bd8b56053dd1d8c69d2
leebackcoding <lcy2825@gmail.com>
2024-11-04 14:32:29 +0900

    feat: 친구 요청 DomainService 구현

--- backend/src/main/java/com/shallwecode/backend/user/domain/service/FriendDomainService.java
+++ backend/src/main/java/com/shallwecode/backend/user/domain/service/FriendDomainService.java
@@ -1,10 +1,13 @@
 package com.shallwecode.backend.user.domain.service;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.shallwecode.backend.user.application.dto.FoundUserDTO;
 import com.shallwecode.backend.user.application.dto.SaveFriendDTO;
 import com.shallwecode.backend.user.application.dto.SaveFriendReqDTO;
 import com.shallwecode.backend.user.application.dto.SaveFriendResDTO;
 import com.shallwecode.backend.user.domain.aggregate.Friend;
+import com.shallwecode.backend.user.domain.aggregate.FriendStatus;
+import com.shallwecode.backend.user.domain.aggregate.QFriend;
 import com.shallwecode.backend.user.domain.repository.FriendRepository;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
@@ -17,10 +20,11 @@ public class FriendDomainService {
     private final FriendRepository friendRepository;
     private final UserDomainService userDomainService;
     private final ModelMapper modelMapper;
+    private final JPAQueryFactory queryFactory;
 
     public SaveFriendResDTO save(SaveFriendReqDTO saveFriendReqDTO) {
 
-        Long loginUserId = 1L;
+        Long loginUserId = 3L;
         FoundUserDTO fromUserDTO = userDomainService.findById(loginUserId);
 
         FoundUserDTO toUserDTO = userDomainService.findById(saveFriendReqDTO.getToUserId());
@@ -36,5 +40,18 @@ public class FriendDomainService {
                 modelMapper.map(toUserDTO, FoundUserDTO.class),
                 savedFriend.getFriendStatus()
         );
+    }
+
+    public boolean isExistFriend(Long loginUserId, Long toUserId) {
+
+        QFriend friend = QFriend.friend;
+
+        long count = queryFactory.selectFrom(friend)
+                .where((friend.fromUser.userId.eq(loginUserId).and(friend.toUser.userId.eq(toUserId)))
+                        .or(friend.fromUser.userId.eq(toUserId).and(friend.toUser.userId.eq(loginUserId))))
+                .where(friend.friendStatus.eq(FriendStatus.PENDING))
+                .fetchCount();
+
+        return count > 0;
     }
 }
