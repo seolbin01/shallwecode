38b47af447206a255928f50c24d58a013e8fe89b
leebackcoding <lcy2825@gmail.com>
2024-11-04 13:40:19 +0900

    feat: 회원 조회에 대한 DTO 추가

---
+++ backend/src/main/java/com/shallwecode/backend/user/application/dto/FoundUserDTO.java
@@ -0,0 +1,18 @@
+package com.shallwecode.backend.user.application.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class FoundUserDTO {
+
+    private Long userId;
+    private String email;
+    private String nickname;
+    private String auth;
+}
