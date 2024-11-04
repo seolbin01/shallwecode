47ebef3e8da45e8987c686b6f5c1a744260f5a20
aosskfdlrla <gmltns885@gmail.com>
2024-11-04 15:46:10 +0900

    feat: 테스트 코드 작성 #36

--- backend/src/test/java/com/shallwecode/backend/problem/domain/service/ProblemDomainServiceTest.java
+++ backend/src/test/java/com/shallwecode/backend/problem/domain/service/ProblemDomainServiceTest.java
@@ -1,6 +1,7 @@
 package com.shallwecode.backend.problem.domain.service;
 
 import com.shallwecode.backend.problem.application.dto.ProblemResDTO;
+import com.shallwecode.backend.problem.application.dto.ProblemResListDTO;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -21,6 +22,15 @@ class ProblemDomainServiceTest {
 
         for(int i = 0; i < problemResList.size(); i++) {
             System.out.println(problemResList.get(i).toString());
+        }
+    }
+
+    @Test
+    void selectProblemList() {
+        List<ProblemResListDTO> problemList = problemDomainService.selectProblemList();
+
+        for(ProblemResListDTO problemResListDTO : problemList) {
+            System.out.println(problemResListDTO.toString());
         }
     }
 }
