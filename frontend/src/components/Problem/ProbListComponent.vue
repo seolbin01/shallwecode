70e14d33e3fd4de0de4028498119191dffb1f99e
aosskfdlrla <gmltns885@gmail.com>
2024-11-05 17:16:08 +0900

    feat: 조회기능 프론트 구현 #43

--- frontend/src/components/Problem/ProbListComponent.vue
+++ frontend/src/components/Problem/ProbListComponent.vue
@@ -1,19 +1,21 @@
 <script setup>
+import SearchBar from "@/components/Problem/SearchBar.vue";
+import ProbListItemComponent from "@/components/Problem/ProbListItemComponent.vue";
 
+const props = defineProps({
+  problemList: {
+    type: Array,
+    required: true
+  },
+  currentPage: Number,
+  totalPages: Number,
+  totalItems: Number
+});
 </script>
 
 <template>
   <!-- 필터바 -->
-  <div class="filter-bar">
-    <select>
-      <option>난이도</option>
-      <option>Lv. 1</option>
-      <option>Lv. 2</option>
-      <option>Lv. 3</option>
-      <option>Lv. 4</option>
-    </select>
-    <input type="text" placeholder="문제 검색">
-  </div>
+  <SearchBar />
 
   <!-- 문제 테이블 -->
   <table class="table">
@@ -27,55 +29,7 @@
     </tr>
     </thead>
     <tbody>
-    <tr>
-      <td>1</td>
-      <td>DFS, BFS</td>
-      <td class="level level-1">Lv. 1</td>
-      <td>수정</td>
-      <td>삭제</td>
-    </tr>
-    <tr>
-      <td>2</td>
-      <td>DFS, BFS</td>
-      <td class="level level-2">Lv. 2</td>
-      <td>수정</td>
-      <td>삭제</td>
-    </tr>
-    <tr>
-      <td>3</td>
-      <td>DFS, BFS</td>
-      <td class="level level-2">Lv. 2</td>
-      <td>수정</td>
-      <td>삭제</td>
-    </tr>
-    <tr>
-      <td>4</td>
-      <td>DFS, BFS</td>
-      <td class="level level-3">Lv. 3</td>
-      <td>수정</td>
-      <td>삭제</td>
-    </tr>
-    <tr>
-      <td>5</td>
-      <td>DFS, BFS</td>
-      <td class="level level-3">Lv. 3</td>
-      <td>수정</td>
-      <td>삭제</td>
-    </tr>
-    <tr>
-      <td>6</td>
-      <td>DFS, BFS</td>
-      <td class="level level-4">Lv. 4</td>
-      <td>수정</td>
-      <td>삭제</td>
-    </tr>
-    <tr>
-      <td>7</td>
-      <td>DFS, BFS</td>
-      <td class="level level-2">Lv. 2</td>
-      <td>수정</td>
-      <td>삭제</td>
-    </tr>
+    <ProbListItemComponent v-for="problem in problemList" :key="problem.problemId" :problem="problem"/>
     </tbody>
   </table>
 
@@ -90,23 +44,6 @@
 </template>
 
 <style scoped>
-.filter-bar {
-  display: flex;
-  justify-content: space-between;
-  margin-bottom: 20px;
-}
-
-.filter-bar select, .filter-bar input {
-  padding: 5px;
-  font-size: 1rem;
-  border: 1px solid #ddd;
-  border-radius: 5px;
-}
-
-.filter-bar input {
-  width: 200px;
-}
-
 .table {
   width: 100%;
   border-collapse: collapse;
@@ -122,15 +59,6 @@
   background-color: #f0f0f0;
   font-weight: bold;
 }
-
-.level {
-  font-weight: bold;
-}
-
-.level-1 { color: #00cc44; }
-.level-2 { color: #0099ff; }
-.level-3 { color: #cc33ff; }
-.level-4 { color: #ff3333; }
 
 .pagination {
   display: flex;
