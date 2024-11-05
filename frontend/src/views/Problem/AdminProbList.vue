92c7adf474797c7dbefb0b61da76f70de80f5107
aosskfdlrla <gmltns885@gmail.com>
2024-11-05 17:15:19 +0900

    feat: 조회기능 프론트 구현 #43

--- frontend/src/views/Problem/AdminProbList.vue
+++ frontend/src/views/Problem/AdminProbList.vue
@@ -1,8 +1,40 @@
 <script setup>
-import SideComponent from "@/components/problem/SideComponent.vue";
-import ProbListComponent from "@/components/problem/ProbListComponent.vue";
+import SideComponent from "@/components/Problem/SideComponent.vue";
+import ProbListComponent from "@/components/Problem/ProbListComponent.vue";
+import {reactive, onMounted, onUnmounted} from "vue"
+import axios from "axios";
 
+/* 필요한 객체 생성부 */
+const adminProblemList = reactive({
+  problemList : [],
+  currentPage: '',
+  totalPages: '',
+  totalItems: ''
+});
 
+/* 필요한 문제 목록 통신 */
+const fetchProblemList = async () => {
+  try {
+    const response = await axios.get(`http://localhost:8080/api/v1/problem/adminList`, {
+      headers : {
+        Authorization : 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJjaGF0QGdtYWlsLmNvbSIsImF1dGgiOlsiUk9MRV9VU0VSIl0sImV4cCI6MTczMDI1MTc5N30.aM3knHn_RuZanzSNI9Hd-dsXIEQaaJUTEWD0fEg-9WCUfb7VahwTzza5e4tQ4EnrtzFPH_TnJsKtgKXqXFAWDQ'
+      },
+      withCredentials : true
+    });
+
+    /* 데이터 매핑 */
+    adminProblemList.problemList = response.data.problemList;
+    adminProblemList.currentPage = response.data.currentPage;
+    adminProblemList.totalPages = response.data.totalPages;
+    adminProblemList.totalItems = response.data.totalItems;
+  } catch (error) {
+    console.error('문제 목록을 불러오는데 문제가 발생했습니다.');
+  }
+}
+// DOM 로드 전 데이터 셋팅
+onMounted(async () => {
+  await fetchProblemList();
+})
 </script>
 
 <template>
@@ -11,7 +43,10 @@ import ProbListComponent from "@/components/problem/ProbListComponent.vue";
     <SideComponent />
     <!-- 메인 컨텐츠 -->
       <div class="content">
-        <ProbListComponent />
+        <ProbListComponent :problemList="adminProblemList.problemList"
+                           :currentPage="adminProblemList.currentPage"
+                           :totalPages="adminProblemList.totalPages"
+                           :totalItem="adminProblemList.totalItems" />
       </div>
   </div>
 </template>
