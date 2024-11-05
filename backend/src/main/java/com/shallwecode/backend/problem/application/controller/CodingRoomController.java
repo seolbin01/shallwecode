4cca9c2d4f0093f1f4c5b393f39e4863f2905cb8
seolbin01 <se01bin@suwon.ac.kr>
2024-11-05 19:41:51 +0900

    fix: 일부 수정

--- backend/src/main/java/com/shallwecode/backend/problem/application/controller/CodingRoomController.java
+++ backend/src/main/java/com/shallwecode/backend/problem/application/controller/CodingRoomController.java
@@ -8,7 +8,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.hibernate.annotations.processing.Find;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
