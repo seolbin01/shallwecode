ff2a11eea0afcf74d4a3313a386ca561d01c4ed0
aosskfdlrla <gmltns885@gmail.com>
2024-11-05 19:56:12 +0900

    feat: 검색바 프론트 작업 #65

--- frontend/src/components/Problem/SearchBar.vue
+++ frontend/src/components/Problem/SearchBar.vue
@@ -1,18 +1,35 @@
 <script setup>
+import { reactive } from 'vue';
 
+/* 검색 객체 생성 */
+const search = reactive({
+  keyword: '',
+  option: ''
+});
+
+/* 부모 이벤트 전달 객체 */
+const emit = defineEmits(['problemSearch']);
+
+/* 부모 컴포넌트로 이벤트 전달 */
+const emitSearch = () => {
+  emit('problemSearch', {
+    option: search.option || null,   // 선택 안되면 null 입력
+    keyword: search.keyword || null
+  });
+};
 </script>
 
 <template>
   <!-- 필터바 -->
   <div class="filter-bar">
-    <select>
-      <option>난이도</option>
-      <option>Lv. 1</option>
-      <option>Lv. 2</option>
-      <option>Lv. 3</option>
-      <option>Lv. 4</option>
+    <select v-model="search.option">
+      <option value="">난이도</option>
+      <option value="1">Lv. 1</option>
+      <option value="2">Lv. 2</option>
+      <option value="3">Lv. 3</option>
+      <option value="4">Lv. 4</option>
     </select>
-    <input type="text" placeholder="검색할 문제 입력">
+    <input type="text" v-model="search.keyword" placeholder="검색할 문제 입력">
     <button @click="problemSearch">검색</button>
   </div>
 </template>
