3cb1b85b0c32de0ed75c9be3bc2930973ce2ba1a
SaeRyung <sr1094@naver.com>
2024-11-06 12:34:48 +0900

    chore: #73 UserController 회원 앤드포인트 swagger 설명 추가, 조회 기능 추가

--- backend/src/main/java/com/shallwecode/backend/user/application/controller/UserController.java
+++ backend/src/main/java/com/shallwecode/backend/user/application/controller/UserController.java
@@ -4,22 +4,28 @@ import com.shallwecode.backend.user.application.dto.UserSaveDTO;
 import com.shallwecode.backend.user.application.dto.UserUpdateDTO;
 import com.shallwecode.backend.user.application.service.UserService;
 import com.shallwecode.backend.user.domain.aggregate.UserInfo;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @Slf4j
 @RestController
 @RequestMapping("/api/v1/user")
+@Tag(name = "User", description = "회원 관련 API")
 public class UserController {
 
     private final UserService userService;
 
     // 회원가입
     @PostMapping("/api/v1/user")
+    @Operation(summary = "회원 가입", description = "회원 가입을 시도한다.")
     public ResponseEntity<String> saveUser(@Valid @RequestBody UserSaveDTO userSaveDTO) {
         userService.saveUser(userSaveDTO);
         return ResponseEntity.ok("회원 가입되었습니다.");
@@ -27,6 +33,7 @@ public class UserController {
 
     // 회원 닉네임 수정
     @PutMapping("/api/v1/user/nickname")
+    @Operation(summary = "회원 닉네임 수정", description = "회원 닉네임을 수정한다.")
     public ResponseEntity<UserInfo> updateUser(@Valid @RequestBody UserUpdateDTO userUpdateDTO) {
         userService.updateUser(userUpdateDTO);
         return ResponseEntity.ok().build();
@@ -34,8 +41,17 @@ public class UserController {
 
     // 회원 삭제
     @DeleteMapping
+    @Operation(summary = "회원 삭제", description = "회원을 삭제한다.")
     public ResponseEntity<String> deleteUser(@RequestParam("userId") Long userId) throws Exception {
         userService.deleteUser(userId);
         return ResponseEntity.ok("회원이 삭제되었습니다.");
+    }
+
+    // 회원 조회
+    @PostMapping
+    @Operation(summary = "회원 전체 조회", description = "전체 회원을 조회한다.")
+    public ResponseEntity<List<UserInfo>> getAllUser() {
+        List<UserInfo> userList = userService.getAllUsers();
+        return ResponseEntity.ok(userList);
     }
 }
