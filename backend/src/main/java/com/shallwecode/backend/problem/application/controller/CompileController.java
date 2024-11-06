0142b24f173a4f95f59e16f5e72b1f23d67bbacd
seolbin01 <se01bin@suwon.ac.kr>
2024-11-06 21:39:43 +0900

    feat: 자바 컴파일 controller 구현

---
+++ backend/src/main/java/com/shallwecode/backend/problem/application/controller/CompileController.java
@@ -0,0 +1,28 @@
+package com.shallwecode.backend.problem.application.controller;
+
+import com.shallwecode.backend.problem.application.dto.CompileReqDTO;
+import com.shallwecode.backend.problem.application.dto.CompileResDTO;
+import com.shallwecode.backend.problem.application.service.CompileService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/compile")
+public class CompileController {
+
+    private final CompileService compileService;
+
+    @PostMapping("/run")
+    public ResponseEntity<CompileResDTO> runCode(@RequestBody CompileReqDTO compileReqDTO) {
+
+        CompileResDTO compileResDTO = compileService.runCode(compileReqDTO);
+
+        return new ResponseEntity<>(compileResDTO, HttpStatus.OK);
+    }
+}
