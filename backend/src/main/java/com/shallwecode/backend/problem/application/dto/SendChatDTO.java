c9e61b2faeb00da7a454ec92eb10e86cf3106daf
aosskfdlrla <gmltns885@gmail.com>
2024-11-07 14:38:27 +0900

    feat: SendChatDTO 생성 #84

---
+++ backend/src/main/java/com/shallwecode/backend/problem/application/dto/SendChatDTO.java
@@ -0,0 +1,19 @@
+package com.shallwecode.backend.problem.application.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+public class SendChatDTO {
+    private Long userId;
+    private String chatContent;
+    private String userNickname;
+    private LocalDateTime sendTime;
+}
