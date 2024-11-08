1131f1e6cc75dc9359627500d30f90f2a3c5492c
leebackcoding <lcy2825@gmail.com>
2024-11-09 05:59:02 +0900

    refactor: 회원가입 프론트 새로 구현

--- frontend/src/views/user/SignUp.vue
+++ frontend/src/views/user/SignUp.vue
@@ -17,62 +17,34 @@
 
 <script setup>
 import {onMounted, ref} from 'vue';
-import { useRouter } from 'vue-router';
-import axios from 'axios'; // axios를 사용하여 HTTP 요청
-import { useAuthStore } from "@/stores/auth.js";
+import {useRoute, useRouter} from 'vue-router';
+import axios from 'axios';
 
-const router = useRouter(); // 라우터 접근
-const nickname = ref(''); // 반응형 데이터 정의
-const email = ref(''); // 이메일을 위한 반응형 데이터 정의
-const store = useAuthStore();
+const router = useRouter();
+const route = useRoute();
+
+const userId = ref(null);
+const nickname = ref('');
+const email = ref('');
 
 const registerUser = async () => {
   try {
-    const accessToken = getCookie('accessToken'); // 로컬 스토리지에서 토큰 가져오기
-    const refreshToken = getCookie('refreshToken');
-    const userId = store.userId; // 예시: userId도 로컬 스토리지에서 가져오는 경우
-    console.log(accessToken);
     console.log(email.value)
     await axios.put('http://localhost:8080/api/v1/user',
         {
           nickname: nickname.value,
-          userId: userId, // userId 추가
-          email: email.value, // 이메일 추가
-        },
-        {
-          headers: {
-            Authorization: `Bearer ${accessToken}`, // Authorization 헤더에 토큰 추가
-            'Authorization-Refresh': refreshToken
-          }
+          userId: userId.value,
+          email: email.value,
         }
     );
-    await router.push('/'); // 메인페이지로 리다이렉션
+    await router.push('/');
   } catch (error) {
     console.error('가입 실패:', error);
   }
 };
 
-function getCookie(name) {
-  const value = `; ${document.cookie}`;
-  const parts = value.split(`; ${name}=`);
-  if (parts.length === 2) return parts.pop().split(';').shift();
-  return null;
-}
-
 onMounted(() => {
-  if (!store.accessToken){
-    console.log("dsfjasklfj;sdjfkadlfs");
-    const token = getCookie('accessToken');  // 쿠키에서 'token' 값 가져오기
-    const token2 = getCookie('refreshToken');
-    if (token) {
-      console.log('쿠키에서 토큰을 가져왔습니다:', token);
-      console.log('리프레시 토큰 : ', token2);
-      store.login(token, token2);  // 로그인설정
-
-    } else {
-      console.log('쿠키에 토큰이 없습니다.');
-    }
-  }
+  userId.value = route.query.userId;
 })
 </script>
 
