10bd422e1da48dfb04b60e26098caff4d7f8366f
leebackcoding <lcy2825@gmail.com>
2024-11-09 05:59:38 +0900

    chore: 불필요한 주석 제거

--- backend/src/main/java/com/shallwecode/backend/user/application/service/UserService.java
+++ backend/src/main/java/com/shallwecode/backend/user/application/service/UserService.java
@@ -52,19 +52,8 @@ public class UserService implements UserDetailsService {
     }
 
 
-//    private final BCryptPasswordEncoder passwordEncoder;
-
-//    @Transactional
-//    public void createUser(CreateUserReqDTO newUser) {
-//        UserEntity user = modelMapper.map(newUser, UserEntity.class);
-//        user.encryptPassword(passwordEncoder.encode(newUser.getPwd()));
-//        userRepository.save(user);
-//    }
-
-    /* 로그인 요청 시 AuthenticationManager를 통해서 호출 될 메소드 */
     @Override
     public UserDetails loadUserByUsername(String userId) throws UsernameNotFoundException {
-        /* 인증 토큰에 담긴 userId가 메소드로 넘어오므로 해당 값을 기준으로 DB에서 조회 한다. */
         UserInfo loginUser = userRepository.findById(Long.parseLong(userId))
                 .orElseThrow(() -> new UsernameNotFoundException(userId));
 
