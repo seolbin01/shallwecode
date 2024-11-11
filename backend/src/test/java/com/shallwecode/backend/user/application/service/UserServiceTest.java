bfef8ced0b2f39db436bef8ff70c053d626edd9a
SaeRyung <sr1094@naver.com>
2024-11-12 03:30:22 +0900

    test: 회원 조회(닉네임, id, 목록) 테스트 코드 구현

--- backend/src/test/java/com/shallwecode/backend/user/application/service/UserServiceTest.java
+++ backend/src/test/java/com/shallwecode/backend/user/application/service/UserServiceTest.java
@@ -1,5 +1,7 @@
 package com.shallwecode.backend.user.application.service;
 
+import com.shallwecode.backend.user.application.dto.user.FindUserDetailDTO;
+import com.shallwecode.backend.user.application.dto.user.FindUserListDTO;
 import com.shallwecode.backend.user.application.dto.user.UserUpdateDTO;
 import com.shallwecode.backend.user.domain.aggregate.UserInfo;
 import com.shallwecode.backend.user.domain.repository.UserRepository;
@@ -9,10 +11,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
@@ -40,6 +42,7 @@ class UserServiceTest {
     @DisplayName("회원 닉네임 수정 테스트")
     void updateUserTest() {
         // 기존 회원 데이터로 닉네임 수정 테스트
+        // given
         Long userId = 71L;
         UserUpdateDTO userUpdateDTO = new UserUpdateDTO();
         userUpdateDTO.setNickname("꺄르륵");
@@ -52,4 +55,45 @@ class UserServiceTest {
         assertEquals("꺄르륵", findUser.get().getNickname());
     }
 
+    @Test
+    @DisplayName("닉네임으로 회원 조회 테스트")
+    void findUserDetailsByNicknameTest() {
+        // 기존 회원 데이터로 닉네임 회원 조회 테스트
+        // given
+        String existUserNick = "Galaxy";
+
+        // when
+        List<FindUserDetailDTO> userNickList = userService.findUserDetailsByNickname(existUserNick);
+
+        // then
+        assertNotNull(userNickList);
+    }
+
+    @Test
+    @DisplayName("ID로 회원 조회 테스트")
+    void findUserDetailsByIdTest() {
+        // 기존 회원 데이터로 아이디 회원 조회 테스트
+        // given
+        Long userId1 = 71L;
+
+        // when
+        userService.findUserDetail(userId1);
+
+        // then
+        assertNotNull(userService.findUserDetail(userId1));
+    }
+
+    @Test
+    @DisplayName("회원 목록 조회 테스트")
+    void findRequestAllUser(){
+        // 기존 회원 데이터, 입력 userID 외 다른 회원 정보 조회
+        // given
+        Long userId = 71L;
+
+        // when
+        List<FindUserListDTO> userList = userService.findRequestUser(userId);
+
+        // then
+        assertNotNull(userList);
+    }
 }
