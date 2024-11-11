f6a5183bb4ebee7512937ff3cf5fb3f640edc577
신민철 <dbeoalscjf98@naver.com>
2024-11-11 15:06:33 +0900

    feat: 유저 삭제시 해당 유저 풀이 시도 목록 삭제 #150

--- backend/src/main/java/com/shallwecode/backend/problem/domain/repository/TryRepository.java
+++ backend/src/main/java/com/shallwecode/backend/problem/domain/repository/TryRepository.java
@@ -4,5 +4,5 @@ import com.shallwecode.backend.problem.domain.aggregate.Try;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TryRepository extends JpaRepository<Try, Long> {
-
+    void deleteByUserId(Long userId);
 }
