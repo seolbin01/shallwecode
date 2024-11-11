f6582783262265b5624050b85ae019e116c71394
SaeRyung <sr1094@naver.com>
2024-11-12 01:30:37 +0900

    test: 회원 닉네임 수정 테스트 코드 구현

--- backend/src/test/java/com/shallwecode/backend/user/application/service/UserServiceTest.java
+++ backend/src/test/java/com/shallwecode/backend/user/application/service/UserServiceTest.java
@@ -1,5 +1,6 @@
 package com.shallwecode.backend.user.application.service;
 
+import com.shallwecode.backend.user.application.dto.user.UserUpdateDTO;
 import com.shallwecode.backend.user.domain.aggregate.UserInfo;
 import com.shallwecode.backend.user.domain.repository.UserRepository;
 import org.junit.jupiter.api.DisplayName;
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
@@ -32,6 +34,22 @@ class UserServiceTest {
         // then
         Optional<UserInfo> findUser = userRepository.findById(71L);
         assertTrue(findUser.isEmpty());
+    }
+
+    @Test
+    @DisplayName("회원 닉네임 수정 테스트")
+    void updateUserTest() {
+        // 기존 회원 데이터로 닉네임 수정 테스트
+        Long userId = 71L;
+        UserUpdateDTO userUpdateDTO = new UserUpdateDTO();
+        userUpdateDTO.setNickname("꺄르륵");
+
+        // when
+        userService.updateUser(userUpdateDTO,userId);
+
+        // then
+        Optional<UserInfo> findUser = userRepository.findById(userId);
+        assertEquals("꺄르륵", findUser.get().getNickname());
     }
 
 }
